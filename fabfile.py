2b75d796d2f0dcd9fa8a40db051300a489ab3a3f
gboone <gregory.boone@cfpb.gov>
2014-03-05 16:09:51 -0500

    Server structure to YAML & deploy checks wp version

--- fabfile.py
+++ fabfile.py
@@ -1,17 +1,31 @@
-from fabric.api import local
+from __future__ import with_statement
+from fabric.api import *
+from fabric.contrib.console import confirm
 import yaml
 
+env.hosts = ['']
 def prepare_deploy():
 	local('git add . && git commit')
 	local('git push')
+
+def stash_and_fetch():
+	local('git stash')
+	local('git fetch')
+
+def checkout_updated():
+	local('git checkout origin')
 
 def hello():
 	print("Hello world!")
 
 def get_config():
-	import yaml
-	config = file('config.yaml', 'r')
-	yaml = yaml.load(config)
+	config = file('config.yaml')
+	data = yaml.load(config)
+	return data
+
+def get_servers():
+	data = get_config()
+	return data['Servers']
 
 def show_themes(data):
 	config = file(data)
@@ -19,3 +33,12 @@ def show_themes(data):
 	for y in data['Themes']:
 		name = y['name']
 		print(name)
+def check_wp_version(wp_dir):
+	with cd(wp_dir):
+		sudo('wp core version')
+
+def deploy():
+	data = get_servers()
+	wp_dir = data['build']['wordpress']
+	with settings(sudo_user="root"):
+		check_wp_version(wp_dir)
